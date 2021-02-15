ba91f6805c264b2721ffcc42602328e4eb832347
Isael Lizama <isaelwebdeveloper@gmail.com>
2021-02-14 19:35:52 -0700

    completed header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import logo from '../../images/logo.svg';
+import './Header.css';
 
 const Header = props => {
     return (
         <header>
-            <img alt='' src={logo} />
-            <nav></nav>
+            <img className='header-logo' alt='Insure logo' src={logo} />
+            <nav>
+                <a className='header-link' href='#'>HOW WE WORK</a>
+                <a className='header-link' href='#'>BLOG</a>
+                <a className='header-link' href='#'>ACCOUNT</a>
+                <a className='header-link view-plans' href='#'>VIEW PLANS</a>
+            </nav>
         </header>
     )
 };
