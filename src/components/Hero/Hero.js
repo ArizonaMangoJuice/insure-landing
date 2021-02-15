e7383c752e96b9b9682b36c2e56ce8fc7ba34bb9
Isael Lizama <isaelwebdeveloper@gmail.com>
2021-02-14 20:11:24 -0700

    created first run of the hero container

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Hero.css';
+import mainImage from '../../images/image-intro-desktop.jpg';
+
+const Hero = props => {
+    return (
+        <section className='hero'>
+            <div className='hero-container'>
+                <div className='hero-text'>
+                    <span className='line'></span>
+                    <h1 className='heading-font hero-text-header'>Humanizing your insurance.</h1>
+                    <p>Get your life insurance coverage easier and faster. We blend our expertise and technology to help you find the plan that's right for you. Ensure you and your loved ones are protected.</p>
+                    <a href='#' className='white-button'>VIEW PLANS</a>
+                </div>
+                <div className='hero-image'>
+                    <img src={mainImage} alt='' />
+                </div>
+            </div>
+        </section>
+    )
+};
+
+export default Hero;
