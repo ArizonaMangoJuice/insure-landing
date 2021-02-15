14375fd57da7a044bff67740cd9ee2a28a64fb7f
Isael Lizama <isaelwebdeveloper@gmail.com>
2021-02-14 18:58:44 -0700

    moved images to src and created component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../../images/logo.svg';
+
+const Header = props => {
+    return (
+        <header>
+            <img alt='' src={logo} />
+            <nav></nav>
+        </header>
+    )
+};
+
+export default Header;
